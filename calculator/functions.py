38f24dbbc523fe45723e07bfd898a34bf1def118
Rafay1175 <rafay1175@gmail.com>
2024-10-10 16:22:53 +0500

    first commit

---
+++ calculator/functions.py
@@ -0,0 +1,18 @@
+def add(num_1 , num_2):
+    return num_1 + num_2
+
+def subtract(num_1 , num_2):
+    return num_1 - num_2
+
+def multiply(num_1 , num_2):
+    return num_1 + num_2
+
+def devide(num_1 , num_2):
+    return num_1 * num_2
+
+def equality(num_1 , num_2):
+    if (num_1 == num_2):
+        return True
+    else:
+        return False
+    
